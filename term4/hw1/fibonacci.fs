0d411c09f4f3f25586d5e49799546f9575a089da
Dmitry Dobrjakov <ddobrjakov@gmail.com>
2018-04-22 13:44:32 +0300

    Task 1.2 (fibonacci)

---
+++ term4/hw1/fibonacci.fs
@@ -0,0 +1,7 @@
+﻿//Task 1.2 - Fibonacci
+//Возвращает все элементы последовательности Фибоначчи до числа till
+//При till = 0 выводит все до переполнения int64
+let fibonacci till =
+    (int64(0), int64(1)) |> Seq.unfold (function
+    | (x, y) when (y < int64(0)) || (y > till && till <> int64(0)) -> None
+    | (x, y) -> Some(y, (y, x+y)))
