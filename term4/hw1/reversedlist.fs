20dd262c48b91bde2204b4d026d3e7cac437b3ac
Dmitry Dobrjakov <ddobrjakov@gmail.com>
2018-04-22 13:48:19 +0300

    Task 1.3 (reversed list)

---
+++ term4/hw1/reversedlist.fs
@@ -0,0 +1,17 @@
+﻿//Task 1.3 - Reversed List
+
+//  starttorev   ending
+//  [1,2,3,4,5]  []
+//  [2,3,4,5]    [1]
+//  [3,4,5]      [2,1]
+//  [4,5]        [3,2,1] 
+//  [5]          [4,3,2,1]
+//  []           [5,4,3,2,1]
+
+let reversed list =
+    let rec revandconc starttorev ending =
+        match starttorev with
+        | [] -> ending
+        | [a] -> a :: ending
+        | _ -> revandconc (starttorev |> List.tail) ((starttorev |> List.head) :: ending)
+    revandconc list []
