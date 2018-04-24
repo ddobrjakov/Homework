6f4ad4d6d169b70b6d2a5cf496d7d5e755d16913
Dmitry Dobrjakov <ddobrjakov@gmail.com>
2018-04-24 10:25:13 +0300

    Task 1 (super map)

---
+++ term4/test1/supermap.fs
@@ -0,0 +1,16 @@
+﻿open NUnit.Framework
+open FsUnit
+
+let superMap list f =
+    let rec supermapAcc resAcc listRest =
+        match listRest with
+        | [] -> resAcc
+        | head::tail -> supermapAcc ((List.rev <| (f head)) @ resAcc) tail
+    List.rev <| supermapAcc [] list
+
+
+[<Test>]
+superMap [1;2;3;4;5] (fun x -> [x; x * x; x * x * x]) |> should equal [1; 1; 1; 2; 4; 8; 3; 9; 27; 4; 16; 64; 5; 25; 125]
+superMap [1;0] (fun x -> [x; x * x; x * x * x]) |> should equal [1; 1; 1; 0; 0; 0]
+superMap [1;2] (fun x -> [x + 1; x; x - 1]) |> should equal [2; 1; 0; 3; 2; 1]
+        
