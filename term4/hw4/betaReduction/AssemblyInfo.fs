9264fca05c300c8de9120d98a4c3a37517fbf609
Dmitry Dobrjakov <ddobrjakov@gmail.com>
2018-06-03 18:45:57 +0300

    Task 4.3

---
+++ term4/hw4/betaReduction/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace beta_reduction.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
+// связанные с этой сборкой.
+[<assembly: AssemblyTitle("beta-reduction")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("beta-reduction")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// При установке значения False в атрибуте ComVisible типы в этой сборке становятся невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте для атрибута ComVisible значение True для этого типа.
+[<assembly: ComVisible(false)>]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[<assembly: Guid("30c08e20-f5d1-4468-936b-66c80731e53f")>]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+// 
+//       Основной номер версии
+//       Дополнительный номер версии 
+//       Номер сборки
+//      Редакция
+// 
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [<сборка: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ term4/hw4/tests/tests4.fs
@@ -0,0 +1,38 @@
+﻿module tests4
+
+open FsUnit
+open NUnit.Framework
+open Reduction
+
+[<Test>]
+let ``Beta-reduction test 1``() =
+    Var 'a' |> betaReduce |> should equal <| Var 'a'
+    
+[<Test>]
+let ``Beta-reduction test 2``() =
+    Application (Abstraction('a', Var 'a' ), Var 'b') |> betaReduce |> should equal <| Var 'b'
+
+[<Test>]
+let ``Beta-reduction test 3``() =
+    Application (Abstraction('a', Application(Var 'a', Var 'c')), Abstraction('x', Application(Var 'b', Var 'x'))) |> betaReduce |> 
+        should equal <| Application (Var 'b', Var 'c')
+
+[<Test>]
+let ``I I = I test``()=
+    let I = Abstraction('x', Var 'x')
+    Application(I, I) |> betaReduce |> should equal I
+
+[<Test>]
+let ``Getting free name test`` () =
+    getFreeName (Abstraction('c', Application(Var 'c', Var 'a'))) (Var 'b') |> should equal 'c'
+
+[<Test>]
+let ``Getting free variables test``() =
+    getFV (Application(Abstraction('x', Application(Var 'b', Var 'x')), Var 'a')) |> should equal ['b'; 'a']
+
+[<Test>]
+let ``Substitution test``() =
+    let A = Abstraction('x', Application(Var 'x', Var 'x'))
+    let B = Abstraction('y', Var 'z')
+    let result = Abstraction('x', Application (Abstraction ('y', Var 'z'), Abstraction ('y', Var 'z')))
+    substitute A 'x' B |> should equal result
