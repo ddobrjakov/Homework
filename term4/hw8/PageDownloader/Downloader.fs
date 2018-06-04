3b5c31f7e1aad28b871896d3c49e12b488999de5
Dmitry Dobrjakov <ddobrjakov@gmail.com>
2018-06-04 06:48:15 +0300

    Task 8

---
+++ term4/hw8/PageDownloader/Downloader.fs
@@ -0,0 +1,62 @@
+﻿module Downloader
+
+open System.Text.RegularExpressions
+open System.Net
+open System
+
+
+/// <summary>
+/// Возвращаемое значение при скачивании страницы (первая строка это адрес, вторая - содержимое)
+/// </summary>
+type DataResult =
+    | SomeData of string * string
+    | NoData of string
+
+(* Возвращает все ссылки в заданном тексте страницы *)
+let findAllLinks pageText =
+    //let urlMask = Regex("<a href=\"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_\+.~#?&\/=]*)\"")
+    let urlMask = Regex("<a href=\"(https?://[^\"]+)\"")
+    let matches = urlMask.Matches(pageText)
+    let Links = seq {
+                        for m in matches do
+                            yield m.Groups.[1].Value
+                    }
+    Links
+
+(* Возвращает для страницы ее адрес и содержимое *)
+let getPageDataAsync url =
+        async
+            {
+                try
+                    let webClient = new WebClient()
+                    let! pageData = webClient.AsyncDownloadString(Uri(url))
+                    return SomeData (url, pageData)
+                with ex -> return NoData (url)
+            }    
+
+(* Возвращает список из адреса и содержимого для каждой страницы включая начальную *)
+let getPagesData mainPage =
+    let pageData = getPageDataAsync mainPage |> Async.RunSynchronously
+    match pageData with
+    | NoData url -> [NoData url]
+    | SomeData (url, data) ->
+        let pageLinks = findAllLinks data
+        let pageDownloads = pageLinks |> Seq.map(fun link -> getPageDataAsync link)
+        pageData :: (Async.Parallel pageDownloads |> Async.RunSynchronously |> Array.toList)
+
+(* Печатает результат скачивания одной страницы *)
+let printInfo pageData =
+    match pageData with
+        | NoData url -> printfn "%s" <| url + " - Error (Не удалось загрузить)"
+        | SomeData (url, text) -> printfn "%s" <| url + " - " + (text |> Seq.length |> string) + " символов"
+
+(* Печатает результат скачивания всех страниц *)
+let handleData pagesData =
+    match pagesData with
+        | mainPageData::otherData ->
+            match mainPageData with
+                | NoData mainURL -> printf "%s" <| mainURL + " - Не удалось скачать данные" 
+                | d -> () //printInfo d
+            if (otherData |> List.isEmpty) then printfn "%s" "Нет ссылок с заданной страницы" 
+            for data in otherData do printInfo data
+        | [] -> ()
---
+++ term4/hw8/Tests/tests8.fs
@@ -0,0 +1,25 @@
+﻿module Tests8
+
+open FsUnit
+open NUnit.Framework
+open Downloader
+
+[<Test>] 
+let ``No links`` () =
+    let url = "http://mahalex.net/151-153/algebra.pdf"
+    let pagesData = getPagesData url
+    pagesData |> List.length |> should equal 1
+
+[<Test>]
+let ``Some links`` () =
+    let url = "http://hwproj.me/courses/9/terms/4"
+    let pagesData = getPagesData url
+    pagesData |> List.length |> should equal 29
+
+[<Test>]
+let ``Incorrect input``() =
+    let url = "not an address"
+    let pageData = getPageDataAsync url |> Async.RunSynchronously
+
+    pageData |> should equal <| NoData url
+    getPagesData url |> List.length |> should equal 1
